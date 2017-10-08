7edb89fd62beb9b87ac1c714ffc665b7ad391505
Carter <mega_narwhal@yahoo.com>
2017-10-07 22:01:03 -0400

    Fall back to no color if Jansi is not present

--- src/main/java/us/blockbox/biomefinder/ColoredConsoleMessager.java
+++ src/main/java/us/blockbox/biomefinder/ColoredConsoleMessager.java
@@ -5,14 +5,33 @@ import org.fusesource.jansi.Ansi;
 import java.util.logging.Logger;
 
 public class ColoredConsoleMessager implements ConsoleMessager{
-
-	private static final String COLOR_WARN = Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.YELLOW).bold().toString();
-	private static final String COLOR_SUCCESS = Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.GREEN).bold().toString();
-	private static final String COLOR_RESET = Ansi.ansi().reset().toString();
+	private static final String COLOR_WARN;
+	private static final String COLOR_SUCCESS;
+	private static final String COLOR_RESET;
 	private final Logger logger;
 
 	public ColoredConsoleMessager(Logger logger){
 		this.logger = logger;
+	}
+
+	static{
+		boolean ansiAvailable = false;
+		try{
+			Class.forName("org.fusesource.jansi.Ansi");
+			ansiAvailable = true;
+		}catch(ClassNotFoundException e){
+			//
+		}
+		if(ansiAvailable){
+			COLOR_WARN = Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.YELLOW).bold().toString();
+			COLOR_SUCCESS = Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.GREEN).bold().toString();
+			COLOR_RESET = Ansi.ansi().reset().toString();
+		}else{
+			final String empty = "";
+			COLOR_WARN = empty;
+			COLOR_SUCCESS = empty;
+			COLOR_RESET = empty;
+		}
 	}
 
 	@Override
